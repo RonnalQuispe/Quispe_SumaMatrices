6177141e2ad3d38724c071126762bb503b00efd1
Laboratorio <laboratorio@udla-ec.int>
2023-11-29 15:52:33 -0500

    asignar numeros aleatorios entre 1 y 100 a la matriz A y B

--- Smatriz.c
+++ Smatriz.c
@@ -14,6 +14,14 @@ int main(int argc, char const *argv[])
     int matrizB[fila][columna];
     int suma[fila][columna];
     srand(time(0));
-   
+    for (i = 0; i < fila; i++)
+    {
+        for (j = 0; j < columna; j++)
+        {
+            matrizA[i][j] = rand() % 101;
+            matrizB[i][j] = rand() % 101;
+        }
+    }
+    
     return 0;
 }
